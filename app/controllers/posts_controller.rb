c6de9a72b7975daeaf0c1a4a343e0fcb6333e3f3
vsai911 <varunsai91@gmail.com>
2015-09-28 03:00:00 +0000

    finished rails tutorial

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+
+
+  def new
+    @post = Post.new
+  end
+
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(
+      title:  params[:post][:title],
+      author: params[:post][:author],
+      text:   params[:post][:text]
+    )
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+ def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '4bd1b6211fd94620dd3856d3270d924df8406209d02d5e4f020c34ca47016b95ce644794f15ec754e9a79819d839e0c02ec1f8c6f0af135ec21684b0cd2f7796'
